c4302cb159aa2182e3e99587b1f1e9f45c9c42db
Ratul <shahoriar.ratul@gmail.com>
2024-02-14 04:27:15 +0600

    update page.tsx and page.tsx

--- app/Header/page.tsx
+++ app/Header/page.tsx
@@ -14,10 +14,7 @@ const navStyle =
   " transition duration-500 ease-in-out rounded-full hover:bg-pink-300/20 p-4";
 export default function Header() {
   return (
-    <div className="fixed z-10 mt-5 flex h-16 w-[80vw] flex-row items-stretch justify-between rounded-full bg-violet-800/25 bg-opacity-45 p-5 text-white transition duration-500 ease-in-out hover:bg-opacity-60 hover:ring-2 hover:ring-inset hover:ring-pink-300">
-      <Link href="#home" className="flex flex-col items-center justify-center">
-        <div className={`${goldman.className} text-xl`}>SHAHORIAR RATUL</div>
-      </Link>
+    <div className="fixed z-10 mt-5 flex h-16 w-[50vw] flex-row  items-stretch justify-center rounded-full bg-violet-800/25 bg-opacity-45 p-5 text-white shadow-lg shadow-indigo-500/50 transition duration-500 ease-in-out hover:bg-opacity-60 hover:ring-2 hover:ring-inset hover:ring-pink-300/35">
       <ul className="flex flex-row items-center justify-center gap-6 ">
         <Link href="#home" className={navStyle}>
           HOME
--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -9,33 +9,33 @@ export default function Hero() {
   return (
     <div
       id="hero"
-      className="relative flex h-screen w-full flex-row items-center justify-start border-b-4 border-black "
+      className="relative flex h-screen w-full flex-col items-center justify-start border-b-4 border-black "
     >
-      <div className=" mx-48 mb-10 mt-4 flex w-[50vw]  flex-col items-center px-12 text-justify text-2xl font-medium !leading-[1.5] sm:text-4xl">
-        <div className="flex items-center justify-center">
-          <div className="relative">
-            <Image
-              src="/potrait.jpeg"
-              alt="Shahoriar Ratul"
-              width="192"
-              height="192"
-              quality="95"
-              priority={true}
-              className="mb-10 h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
-            />
-            <div className=" absolute bottom-0 right-0 text-4xl">👋</div>
-          </div>
+      <div className=" h-[600px] w-[600px] ">
+        <Canvas />
+      </div>
+      <div className=" text mx-48 mb-10 mt-[-30px] flex  w-[60vw] flex-col items-center px-12 text-center text-2xl font-medium !leading-[1.5] sm:text-4xl">
+        <div className="mb-10  flex items-center justify-center">
+          <Image
+            src="/potrait.jpeg"
+            alt="Shahoriar Ratul"
+            width="220"
+            height="220"
+            quality="95"
+            priority={true}
+            className="mr-10 inline-block h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl "
+          />
+          <h1 className="text-left">
+            <span className="font-bold"> Hello, I&apos;m Ratul.</span> I&apos;m
+            a <span className="font-bold">full-stack developer</span> with{" "}
+            <span className="font-bold">hands on </span> of experience on
+            multiple pojects. I enjoy building{" "}
+            <span className="italic">sites & apps </span>
+            that feels alive . My focus is{" "}
+            <span className="underline">React</span> <span> on </span>{" "}
+            <span className="underline"> Next.js</span>.
+          </h1>
         </div>
-        <h1 className="text-left">
-          <span className="font-bold">Hello, I&apos;m Ratul.</span> I&apos;m a{" "}
-          <span className="font-bold">full-stack developer</span> with{" "}
-          <span className="font-bold">hands on </span> of experience on multiple
-          pojects. I enjoy building{" "}
-          <span className="italic">sites & apps </span>
-          that feels alive . My focus is{" "}
-          <span className="underline">React</span> <span> on </span>{" "}
-          <span className="underline"> Next.js</span>.
-        </h1>
         <Link
           href="#footer"
           className=" rounded-full bg-[#8e5196] p-3 text-center text-xl text-white transition duration-300 ease-in-out hover:scale-110"
@@ -45,10 +45,6 @@ export default function Hero() {
             <BsArrowRight className="inline-block opacity-70 transition group-hover:translate-x-2" />
           </span>
         </Link>
-      </div>
-
-      <div className="absolute right-52 top-44 h-[600px] w-[600px] ">
-        <Canvas />
       </div>
     </div>
   );
