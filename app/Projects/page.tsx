1bec2cbfb1689eb107ae6097cafa73cd277d0d95
Ratul <shahoriar.ratul@gmail.com>
2024-02-29 04:38:44 +0600

    update page.tsx, ProjectCard.tsx, 'project 1.png' and 'project 2.png'

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -3,12 +3,29 @@ import React from "react";
 
 export default function Work() {
   return (
-    <div id="projects" className="flex h-screen w-full justify-center">
-      <div className="flex flex-col items-center sm:mt-16 sm:grid sm:grid-cols-2 sm:gap-20 sm:gap-y-2">
-        <ProjectCard src="/project 1.png">Project 1</ProjectCard>
-        <ProjectCard src="/project 2.png">Project 2</ProjectCard>
-        <ProjectCard src="/project 1.png">Project 3</ProjectCard>
-        <ProjectCard src="/project 1.png">Project 4</ProjectCard>
+    <div
+      id="projects"
+      className="flex h-screen w-full justify-center sm:h-[130vh]"
+    >
+      <div className="flex flex-col items-center sm:mt-16 sm:grid sm:grid-cols-1 sm:gap-20 sm:gap-y-2">
+        <ProjectCard
+          link="https://fast-pizza-sigma.vercel.app/"
+          src="/project 1.png"
+        >
+          Project 1
+        </ProjectCard>
+        <ProjectCard
+          link="https://use-pop-corn-ebon.vercel.app/"
+          src="/project 2.png"
+        >
+          Project 2
+        </ProjectCard>
+        <ProjectCard
+          link="https://travel-list-lovat.vercel.app/"
+          src="/project 3.png"
+        >
+          Project 3
+        </ProjectCard>
       </div>
     </div>
   );
